8d6284afcc14423ec20abed901a75bbf9be52249
Adam Nemecek <adamnemecek@gmail.com>
2020-09-11 14:24:55 -0700

    use self

--- src/size.rs
+++ src/size.rs
@@ -14,7 +14,7 @@ impl<T> Size<T> {
     ///
     /// [`Size`]: struct.Size.html
     pub const fn new(width: T, height: T) -> Self {
-        Size { width, height }
+        Self { width, height }
     }
 }
 
@@ -22,23 +22,23 @@ impl Size {
     /// A [`Size`] with zero width and height.
     ///
     /// [`Size`]: struct.Size.html
-    pub const ZERO: Size = Size::new(0., 0.);
+    pub const ZERO: Self = Self::new(0., 0.);
 
     /// A [`Size`] with a width and height of 1 unit.
     ///
     /// [`Size`]: struct.Size.html
-    pub const UNIT: Size = Size::new(1., 1.);
+    pub const UNIT: Self = Self::new(1., 1.);
 
     /// A [`Size`] with infinite width and height.
     ///
     /// [`Size`]: struct.Size.html
-    pub const INFINITY: Size = Size::new(f32::INFINITY, f32::INFINITY);
+    pub const INFINITY: Self = Self::new(f32::INFINITY, f32::INFINITY);
 
     /// Increments the [`Size`] to account for the given padding.
     ///
     /// [`Size`]: struct.Size.html
     pub fn pad(&self, padding: f32) -> Self {
-        Size {
+        Self {
             width: self.width + padding * 2.0,
             height: self.height + padding * 2.0,
         }
@@ -47,12 +47,12 @@ impl Size {
 
 impl From<[f32; 2]> for Size {
     fn from([width, height]: [f32; 2]) -> Self {
-        Size { width, height }
+        Self { width, height }
     }
 }
 
 impl From<[u16; 2]> for Size {
     fn from([width, height]: [u16; 2]) -> Self {
-        Size::new(width.into(), height.into())
+        Self::new(width.into(), height.into())
     }
 }
